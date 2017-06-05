899e59d24811f358144488a8d22a902623e95f44
Donrockz <rockstarrockey18@gmail.com>
2017-06-06 00:19:49 +0530

    Add files via upload

---
+++ Assignment 2.1/src/PrintWithoutSemicolon.java
@@ -0,0 +1,12 @@
+
+public class PrintWithoutSemicolon {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+
+		for (i = 0; i < 1; i++) {
+			System.out.println("Rakesh Anantheswaran");
+		}
+	}
+}
